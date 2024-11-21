a8e63074e306f2fd1d599f9739ec97b10c7f3677
tothedistance <tothedistance@mailtothedistance.com>
2024-11-21 15:04:56 +0800

    hw0

--- hw0/main.cpp
+++ hw0/main.cpp
@@ -1,24 +1,20 @@
-#include<cmath>
-#include<eigen3/Eigen/Core>
-#include<eigen3/Eigen/Dense>
-#include<iostream>
+#include "common.h"
 
-int main(){
-
+int f() {
     // Basic Example of cpp
     std::cout << "Example of cpp \n";
     float a = 1.0, b = 2.0;
     std::cout << a << std::endl;
-    std::cout << a/b << std::endl;
+    std::cout << a / b << std::endl;
     std::cout << std::sqrt(b) << std::endl;
     std::cout << std::acos(-1) << std::endl;
-    std::cout << std::sin(30.0/180.0*acos(-1)) << std::endl;
+    std::cout << std::sin(30.0 / 180.0 * acos(-1)) << std::endl;
 
     // Example of vector
     std::cout << "Example of vector \n";
     // vector definition
-    Eigen::Vector3f v(1.0f,2.0f,3.0f);
-    Eigen::Vector3f w(1.0f,0.0f,0.0f);
+    Eigen::Vector3f v(1.0f, 2.0f, 3.0f);
+    Eigen::Vector3f w(1.0f, 0.0f, 0.0f);
     // vector output
     std::cout << "Example of output \n";
     std::cout << v << std::endl;
@@ -33,16 +29,32 @@ int main(){
     // Example of matrix
     std::cout << "Example of matrix \n";
     // matrix definition
-    Eigen::Matrix3f i,j;
+    Eigen::Matrix3f i, j;
     i << 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0;
     j << 2.0, 3.0, 1.0, 4.0, 6.0, 5.0, 9.0, 7.0, 8.0;
     // matrix output
     std::cout << "Example of output \n";
     std::cout << i << std::endl;
     // matrix add i + j
+    // cout << i + j << endl;
     // matrix scalar multiply i * 2.0
+    // cout << i * 2.0 << endl;
     // matrix multiply i * j
+    cout << i * j << endl;
     // matrix multiply vector i * v
+    cout << i * v << endl;
 
+    return 0;
+}
+
+int main(int argc, char const *argv[]) {
+    Vector3f v(2, 1, 1);
+    Matrix3f m;
+    m << cosf(PI / 4), -sinf(PI / 4), 0,
+        sinf(PI / 4), cosf(PI / 4), 0,
+        0, 0, 1;
+    v = m * v + Vector3f(1, 2, 0);
+    cout << m << endl;
+    cout << m * v << endl;
     return 0;
 }
